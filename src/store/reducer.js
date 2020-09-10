75e9d3dd1b01d489a4d5a16180906385de651c79
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-10 16:52:46 +0200

    refactor switch case in the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,29 +4,27 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return {
-      ...state,
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "DECREMENT") {
-    return {
-      ...state,
-      counter: state.counter - 1,
-    };
-  }
-  if (action.type === "ADD") {
-    return {
-      ...state,
-      counter: state.counter + 5,
-    };
-  }
-  if (action.type === "SUBTRACT") {
-    return {
-      ...state,
-      counter: state.counter - 5,
-    };
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "ADD":
+      return {
+        ...state,
+        counter: state.counter + action.value,
+      };
+    case "SUBTRACT":
+      return {
+        ...state,
+        counter: state.counter - action.value,
+      };
   }
   return state;
 };
