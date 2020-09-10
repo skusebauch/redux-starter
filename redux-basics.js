1a5ea571d7bc42af05bf18d7a68abbaf2942ad8e
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-10 15:39:56 +0200

    dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,7 +7,19 @@ const initialState = {
 };
 
 // Reducer
-const rootReducer = (state = 0, action) => {
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 };
 
@@ -16,5 +28,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 5 });
+console.log(store.getState());
 
 // Subsription
