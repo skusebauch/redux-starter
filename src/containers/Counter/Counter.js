9767d2b7ba7692e5c462a73de484ce1198021ae7
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-12 10:48:06 +0200

    refactor file restructuring

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import * as actionCreators from "../../store/actions/actions";
+import * as actionCreators from "../../store/actions/index";
 
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
-import * as actionTypes from "../../store/actions/actions";
 
 class Counter extends Component {
   counterChangedHandler = (action, value) => {
